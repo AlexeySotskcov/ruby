9c0404834acdf20d3244a32381d44c98932b6f8c
AlexeySotskcov <sotskcov.aleksei@yandex.by>
2018-02-08 11:00:26 +0300

    page title refactoring
    used helper; refactoring tests (adding method setup);  added and used gem for better perception of test text

--- rails/first_app/app/helpers/application_helper.rb
+++ rails/first_app/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+
+    base_title = "My first application"
+
+    if page_title.empty?
+      return base_title
+    end
+
+    page_title + " | " + base_title
+
+  end
 end
--- rails/first_app/test/controllers/static_pages_controller_test.rb
+++ rails/first_app/test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @title = "My first application"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", @title
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help"
+    assert_select "title", "Help" + " | " + @title
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About"
+    assert_select "title", "About" + " | " + @title
   end
 end
