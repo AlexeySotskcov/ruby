220e126d9944ef25f42212748b133f3500da2aaf
AlexeySotskcov <sotskcov.aleksei@yandex.by>
2018-02-08 12:50:31 +0300

    add integration test
    added integration test, edited static_pages_controller_test, edited partial _header for using root_path instead home_path

--- rails/first_app/test/controllers/static_pages_controller_test.rb
+++ rails/first_app/test/controllers/static_pages_controller_test.rb
@@ -9,15 +9,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get '/'
     assert_response :success
-    assert_select "title", @title  
+    assert_select "title", @title
   end
 
+=begin
   test "should get home" do
     #get static_pages_home_url
     get home_path #home_path - /home ; home_url - full url
     assert_response :success
     assert_select "title", @title
   end
+=end
 
   test "should get help" do
     #get static_pages_help_url
