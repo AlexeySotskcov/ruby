e1c2b8aa50246e11ff08297b6b6a597ec8d11aa2
AlexeySotskcov <sotskcov.aleksei@yandex.by>
2018-02-10 10:50:03 +0300

    writing test for new page

--- rails/first_app/test/controllers/user_controller_test.rb
+++ rails/first_app/test/controllers/user_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @title = "My first application"
+  end
+
   test "should get new" do
-    get user_new_url
+    get signup_path
     assert_response :success
+    assert_select "title", "Sign Up" + " | " + @title
   end
 
 end
