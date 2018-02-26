e4ff984361b3b75cae46c2b442e6ae994d1c1e3c
AlexeySotskcov <sotskcov.aleksei@yandex.by>
2018-02-26 15:07:28 +0300

    add password reset

--- rails/first_app/db/schema.rb
+++ rails/first_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226061846) do
+ActiveRecord::Schema.define(version: 20180226105231) do
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'bolean' for column 'activated'
