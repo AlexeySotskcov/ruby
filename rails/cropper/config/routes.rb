cbeee14cea25130ecdba812374c9a358f0844db5
AlexeySotskcov <31882728+AlexeySotskcov@users.noreply.github.com>
2018-03-07 17:40:38 +0300

    Update routes.rb

--- rails/cropper/config/routes.rb
+++ rails/cropper/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "url_crops#new"
-  resources :url_crops, only: [:new, :create]
+  resources :url_crops
   # redirect if unknown url
   get '*path', to: 'url_crops#must_be_redirected'
 end
